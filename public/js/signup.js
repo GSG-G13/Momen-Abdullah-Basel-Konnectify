b7c96c6366b7a6f03eb6b736a2648eac7e79329c
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-04-12 23:17:10 +0300

    Done the uploud user image

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,13 +4,11 @@ signUpButton.addEventListener("click", (event) => {
   event.preventDefault();
 
   const person_name = document.getElementById("person_name").value;
-
   const username = document.getElementById("username").value;
-
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const profile_image = document.getElementById("profile_image").value;
-  const bg_img_url = document.getElementById("bg_img_url").value;
+  const profile_image = document.getElementById("profile_image").files[0];
+  const bg_img_url = document.getElementById("bg_img_url").files[0];
   const bio = document.getElementById("bio").value;
   const skills = document.getElementById("skills").value;
 
@@ -34,12 +32,14 @@ signUpButton.addEventListener("click", (event) => {
   }
 
   const emailError = document.getElementById("email_error");
+  
   if (!email.match(/^\S+@\S+\.\S+$/)) {
     emailError.innerHTML = "Please enter a valid email address";
     isValid = false;
   } else {
     emailError.innerHTML = "";
   }
+  
 
   const passwordError = document.getElementById("password_error");
   if (password.trim() === "") {
@@ -50,16 +50,16 @@ signUpButton.addEventListener("click", (event) => {
   }
 
   const profileImageError = document.getElementById("profile_image_error");
-  if (profile_image.trim() === "") {
-    profileImageError.innerHTML = "Please enter a profile image URL";
+  if (!profile_image) {
+    profileImageError.innerHTML = "Please select a profile image";
     isValid = false;
   } else {
     profileImageError.innerHTML = "";
   }
 
   const bgImgError = document.getElementById("bg_img_url_error");
-  if (bg_img_url.trim() === "") {
-    bgImgError.innerHTML = "Please enter a background image URL";
+  if (!bg_img_url) {
+    bgImgError.innerHTML = "Please select a background image";
     isValid = false;
   } else {
     bgImgError.innerHTML = "";
@@ -75,23 +75,19 @@ signUpButton.addEventListener("click", (event) => {
 
   // Submit data if all inputs are valid
   if (isValid) {
-    const data = {
-      person_name,
-      username,
-      email,
-      password,
-      profile_image,
-      bg_img_url,
-      bio,
-      skills,
-    };
+    const data = new FormData();
+    data.append("person_name", person_name);
+    data.append("username", username);
+    data.append("email", email);
+    data.append("password", password);
+    data.append("profile_image", profile_image);
+    data.append("bg_img_url", bg_img_url);
+    data.append("bio", bio);
+    data.append("skills", skills);
 
     fetch("/signup", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      body: data,
     })
       .then((response) => {
         if (response.ok) {
