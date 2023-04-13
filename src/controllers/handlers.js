1cb3da0c1caa0abde12d28c11a2af53127093d1d
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-04-13 04:46:05 +0300

    Some editing

--- src/controllers/handlers.js
+++ src/controllers/handlers.js
@@ -34,14 +34,16 @@ const addUserSchema = Joi.object({
 
 // Multer middleware to handle file uploads
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const fileExt = file.originalname.split('.').pop();
+      cb(null, uniqueSuffix + '.' + fileExt);
+    }
+  });
+  
 
 const upload = multer({ storage: storage });
 
